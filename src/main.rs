baf1c8c6e1a4999f3968490ae444e4cd452ae9a1
Akiva Goldman <akivazev@gmail.com>
2025-03-18 11:38:11 +0200

    Implemented interactive CLI shift printing program using shift_maker library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,81 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io};
+use shift_maker::*;
+use util::input_time;
+use time::{Time, Duration};
+
+fn print_fixed_shift_list() {
+    let mut input = String::new();
+
+    println!("Enter start time (HH or HH:MM):");
+
+    let start_time_str = input_time().expect("Invalid start time");
+
+    let start_time = Time::from_string(&start_time_str).expect("Invalid start time");
+
+    println!("Enter end time (HH or HH:MM):");
+
+    let end_time_str = input_time().expect("Invalid end time");
+
+    let end_time = Time::from_string(&end_time_str).expect("Invalid end time");
+
+    println!("Enter shift length (HH or HH:MM):");
+
+    let shift_length_str = input_time().expect("Invalid shift length");
+
+    let shift_length = Duration::from_string(&shift_length_str).expect("Invalid shift length");
+
+    println!("Enter names separated by whitespace:");
+
+    io::stdin().read_line(&mut input)
+        .expect("Input error");
+
+    let names: Vec<&str> = input.split_whitespace().collect();
+
+    let shift_list = interval_fixed_shift_list(&names, shift_length, start_time, end_time);
+
+    for (name, start, end) in shift_list {
+        println!("{}-{} {}", start, end, name);
+    }
 }
+
+fn print_shift_list() {
+    let mut input = String::new();
+
+    println!("Enter start time (HH or HH:MM):");
+
+    let start_time_str = input_time().expect("Invalid start time");
+
+    let start_time = Time::from_string(&start_time_str).expect("Invalid start time");
+
+    println!("Enter end time (HH or HH:MM):");
+
+    let end_time_str = input_time().expect("Invalid end time");
+
+    let end_time = Time::from_string(&end_time_str).expect("Invalid end time");
+
+    println!("Enter names separated by whitespace:");
+
+    io::stdin().read_line(&mut input)
+        .expect("Input error");
+
+    let names: Vec<&str> = input.split_whitespace().collect();
+
+    let shift_list = interval_var_shift_list(&names, start_time, end_time);
+
+    for (name, start, end) in shift_list {
+        println!("{}-{} {}", start, end, name);
+    }
+}
+
+fn main() {
+
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 && args[1] == "--fixed-shift" {
+        print_fixed_shift_list();
+    } else {
+        print_shift_list();
+    }
+}
+
